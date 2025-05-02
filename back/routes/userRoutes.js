417a64545624537d3651a9d7cde8f092d9f1b96b
Mariana0908 <marianaospinahenao0908@gmail.com>
2025-05-01 21:20:30 -0500

    Definition of the logic for swipes and matches

--- back/routes/userRoutes.js
+++ back/routes/userRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const swipesController = require('../controllers/swipesController');
+const matchesController = require('../controllers/matchesController');
+
 
 router.post('/', userController.createUser);
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
+router.post('/:id/like', swipesController.saveLike);
+router.post('/:id/dislike', swipesController.saveDislike);
+router.get('/:id/matches', matchesController.getMatches);
+
 
 module.exports = router;
